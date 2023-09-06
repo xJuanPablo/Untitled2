67ddeaf737127c446ae002b895544964bb27c635
micavilla <micaella.villa1@gmail.com>
2023-09-06 12:51:26 -0500

    started models folder files

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
+  fountains: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Fountain'
+    }
+  ]
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
