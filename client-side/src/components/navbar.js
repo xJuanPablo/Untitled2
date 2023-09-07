536735f6bfea6ca90e05bbef01633cb9750a24fa
drog41813 <daniel.lee042003@gmail.com>
2023-09-07 11:01:24 -0500

    got navbar set up for guest user

--- client-side/src/components/navbar.js
+++ client-side/src/components/navbar.js
@@ -51,16 +51,13 @@ export const NavBar = () =>{
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills"className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects"className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#skills"className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('register')}>Register</Nav.Link>
+              <Nav.Link href="#projects"className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('save new location')}>Save new location</Nav.Link>
+              <Nav.Link href="#projects"className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('login')}>Login</Nav.Link>
             </Nav>
             <span className="navbar-text">
-                <div className="social-icon">
-                    <a href="#"  target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt=""/></a>
-                    <a href="#" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt=""/></a>
-                </div>
                 <a href="#">
-                <button className="vvd" onClick={()=> console.log('connect')}><span>coming soon</span></button>
+                <button className="vvd" onClick={()=> console.log('connect')}><span>Add new location</span></button>
                 </a>
             </span>
           </Navbar.Collapse>
