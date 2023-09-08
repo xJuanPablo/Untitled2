600f5f3b5a7909c819711c2ab7b2c4a0bada2ea2
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-09-08 13:41:59 -0500

    added bottom sheet, still need to fix up and update

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,19 +1,29 @@
-//import logo from './logo.svg';
-import {NavBar} from './components/navbar';
-import { Signup } from './components/signup';
-import { Home } from './components/home';
+import { BottomSheet } from "react-spring-bottom-sheet";
+import React, { useState } from "react";
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="App">
-      {/* components will go here */}
-      <NavBar />
-      <Signup />    
-      <Home />
-    </div>
+    <>
+      <div className="main">
+        MAIN CONTENT
+        <button onClick={() => setOpen(!open)}>
+          {open ? "CLOSE" : "OPEN"} SHEET
+        </button>
+      </div>
+      <div className="footer">YOUR MAIN FOOTER</div>
+      <BottomSheet
+        open={open}
+        onDismiss={() => setOpen(false)}
+        header={<div className="sheetHeader">SHEET HEADER</div>}
+        snapPoints={({ maxHeight }) => 0.9 * maxHeight}
+        sibling={<div className="sheetFooter">YOUR MAIN FOOTER</div>}
+      >
+        <div className="sheetBody">SHEET BODY</div>
+      </BottomSheet>
+    </>
   );
 }
 
-export default App;
