26710c8d804202ea8f19a72a3d687796acefdb98
drog41813 <daniel.lee042003@gmail.com>
2023-09-11 14:27:25 -0500

    readjusted App function in App.js

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,5 +1,4 @@
 import {NavBar} from './components/navbar';
-import { Signup } from './components/signup';
 // import { Home } from './components/home';
 // import React, { useState } from "react";
 import { Map } from './components/Map';
@@ -9,14 +8,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      {/* was testing map :D */}
-      <Map/>
-      {/* <Signup /> */}
-      <SlideUp />
-    </div>
-  );
-}
+    <ApolloProvider client = {client}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route
+              path = "/components"
+              element = {<NavBar />}
+            />
+            <Route
+              path = "/components"
+              element = {<Map />}
+            />
+            <Route 
+              path = "/components"
+              element = {<SlideUp />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+    
+)}
 
 export default App;
+// import {NavBar} from './components/navbar';
+// import { Signup } from './components/signup';
+// // import { Home } from './components/home';
+// // import React, { useState } from "react";
+// import { Map } from './components/Map';
+// import SlideUp from './components/slideup';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <NavBar />
+//       {/* was testing map :D */}
+//       <Map/>
+//       {/* <Signup /> */}
+//       <SlideUp />
+//     </div>
+//   );
+// }
+
+// export default App;
