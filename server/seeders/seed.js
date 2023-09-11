0e8670a6d09a4feeb7122e86d6e6934a117f40c1
ColtonSiegmund <135175571+ColtonSiegmund@users.noreply.github.com>
2023-09-11 12:34:48 -0500

    added user seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,10 +1,14 @@
 const db = require('../config/connection');
+const { User } = require('../models');
 const Fountain = require('../models/Fountain')
 const fountainSeeds = require('./demoFountain.json')
+const userSeeds = require('./userSeeds.json')
 
 db.once('open', async () => {
   await Fountain.deleteMany({});
+  await User.deleteMany({});
 
+  await User.create(userSeeds);
   const technologies = await Fountain.insertMany(fountainSeeds);
 
   console.log('Technologies seeded!');
