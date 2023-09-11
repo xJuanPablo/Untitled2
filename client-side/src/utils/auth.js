bae0aab1220d500e3f9ce669925a28c44b3b0283
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-09-11 14:42:18 -0500

    populated auth.js client side

---
+++ client-side/src/utils/auth.js
@@ -0,0 +1,24 @@
+
+
+function setToken(token){
+    localStorage.setItem('token', token);
+}
+
+function getToken(){
+    return localStorage.getItem('token');
+}
+
+function removeToken(){
+    localStorage.removeItem('token');
+}
+
+function isAuthenticated(){
+        const token = getToken();
+
+        if(token){
+            return true;
+        }
+        return false;
+}
+
+export {setToken, getToken, removeToken, isAuthenticated};
