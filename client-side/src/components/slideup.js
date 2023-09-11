fd6f095d5cff65e6aaf8c585f58ea0c1e85925fc
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-09-11 09:55:18 -0500

    sending this push just in case

--- client-side/src/components/slideup.js
+++ client-side/src/components/slideup.js
@@ -4,43 +4,46 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import 'react-spring-bottom-sheet/dist/style.css'
 
 export default function SlideUp() {
-  const [open, setOpen] = useState(true); // Set the initial state to true to make the sheet open by default
-
+  const [open, setOpen] = useState(true);
   return (
     <>
-      <div className="main">
-        MAIN CONTENT
-      </div>
-      <div className="footer">YOUR MAIN FOOTER</div>
-      <BottomSheet
+      <BottomSheet className='slideup'
         open={open}
-        onDismiss={() => setOpen(false)}
-        header={<div className="sheetHeader">SHEET HEADER
-        <Container className="mt-5">
-          <Row>
-            <Col sm={4}>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button>
-                  Search
-                </Button>
-              </Form>
-          </Col>
-        </Row>
-      </Container>
-      </div>}
-        snapPoints={({ maxHeight }) => 0.9 * maxHeight}
+        // had to change setOpen to true in order for bottom sheet to not go all the way down
+        // original version had setOpen set to false
+        onDismiss={() => setOpen(true)}
+        header={
+          <div className="sheetHeader">SHEET HEADER
+            <Container className="mt-5">
+              <Row>
+                <Col sm={4}>
+                  <Form className="d-flex">
+                    <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"
+                    />
+                    <Button>
+                      Search
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        }
+        snapPoints={({ maxHeight }) => {
+          const popHeight = 780; // Adjust the pop height as needed
+          return [maxHeight - popHeight, maxHeight - 200]; // Set the pop and bottom gap heights
+        }}
       >
         <div className="sheetBody">SHEET BODY</div>
       </BottomSheet>
     </>
   );
 }
+
 
 
 
